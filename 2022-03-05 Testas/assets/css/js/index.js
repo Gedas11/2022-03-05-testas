364ebe1e90a9482fdcadd4b452b901007842fe0c
Gedas11 <97019328+Gedas11@users.noreply.github.com>
2022-03-05 11:08:43 +0100

    2022-03-05 Testas
    prastokai

---
+++ 2022-03-05 Testas/assets/css/js/index.js
@@ -0,0 +1,30 @@
+const africa = ['Zebras', 'Liūtas',  '', 'Raganosis', '','Raganosis', 'Begemotas'];
+const australia = ['Kengūra', 'Ančiasnapis', 'Dingo', 'Atsirado', 'Strutis'];
+
+
+function rand(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+//------------ 1 uzduotis---------
+//--------------A--------------
+let a = rand(1, 6);
+document.getElementsByTagName('h2').addEventListener('click')
+
+//---------------B--------------
+if(a === a){
+    document.getElementsByTagName('h2').style.color = 'red'
+}
+
+//---------- 2 uzduotis------------
+//--------------- A ---------------
+let mas = []
+let b = rand(1, 10);
+document.getElementsByTagName('h3' + []).innerHTML 
+
+
+//------------ B ----------------
+
+
+let Suma =  document.getElementsByTagName('h3').innerHTML
